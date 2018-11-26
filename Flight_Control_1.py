b225b699e9b3a9e908311649e7088bc353a59a59
blakethompson82 <44252687+blakethompson82@users.noreply.github.com>
2018-11-26 12:42:03 -0600

    Add files via upload

--- Flight_Control_1.py
+++ Flight_Control_1.py
@@ -16,25 +16,25 @@ def Next_Point(distance, angle, lat1, long1):
 
     #Calculates length between each degree of longitude, based on latitude coordinate
 
-    arc_length_per_degree = 111317.4306 #arc length per degree of earth's circumference (meters)
+	arc_length_per_degree = 111317.4306 #arc length per degree of earth's circumference (meters)
 
-    long_difference = math.cos(lat1) * arc_length_per_degree	#dependent on latitude
-
-
+	long_difference = math.cos(lat1) * arc_length_per_degree	#dependent on latitude
 
 
-    dx = distance * math.sin(angle)     # Change in x direction
-    dy = distance * math.cos(angle)     # Change in y direction
 
-    delta_longitude = dx / (111131.5 * math.cos(lat1)) # Converts change in x to change in longitude [degrees]
+
+	dx = distance * math.sin(angle)     # Change in x direction
+	dy = distance * math.cos(angle)     # Change in y direction
+
+	delta_longitude = dx / (111131.5 * math.cos(lat1)) # Converts change in x to change in longitude [degrees]
 
         # Distance between each degree of latitude varies between 110949 [m] and 111314 [m].
         # Average of the two used since distance differences of project is so small
 
-    delta_latitude = dy / long_difference   # Converts change in y to change in latitude [degrees]
+	delta_latitude = dy / long_difference   # Converts change in y to change in latitude [degrees]
 
-    long2 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
-    lat2 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
+	long2 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
+	lat2 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
 
 	list = [lat2,long2]
 
@@ -43,7 +43,7 @@ def Next_Point(distance, angle, lat1, long1):
 
 antenna_frequency = float(input("Antenna Frequency: "))
 
-antenna_length = float(input("Antenna length"))
+antenna_length = float(input("Antenna length: "))
 
 wavelength = ( 3 * (10 ** 8)) / antenna_frequency
 
@@ -106,7 +106,7 @@ while vehicle.channels['5'] >= 1400:		#if manual mode is on and antenna location
 	# A switch on the remote will be used to toggle drone being controlled
 	# by the user or the Raspberry Pi
 
-	if vehicle.channels['6'] >= 1400
+	if vehicle.channels['6'] >= 1400:
 		lat_ant = vehicle.location.global_frame.lat	#sets antenna latitude
 		long_ant = vehicle.location.global_frame.long	#sets antenna londitude
 		alt_ant = vehicle.location.global_frame.alt	#sets antenna altitude
@@ -133,19 +133,48 @@ while vehicle.channels['5'] < 1400:		#if Channel 5 is off
 	vehicle.simple_goto(point_1, 0.5)	# Commands the drone to go the the desired location at 0.5 m/s
 
 	z = 0								# variable for incrementing
-	point_list = [0] * 60			# creates a list with 60 entries
+	point_list_arc1 = [0] * 60			# creates a list with 60 entries
+	point_list_arc1[0] = point_1
+	for i in range(148,88,-2):
+		p1x_1 = far_field * math.sin(60)				#lines 139-145 are used to find the necessary distance to travel to next point on the first arc
+		p1y_1 = far_field * math.cos(60)
+		p2x_1 = far_field * math.sin(58)
+		p2y_1 = far_field * math.cos(58)
+		delta_x_1 = p2x_1 - p1x_1
+		delta_y_1 = p2y_1 - p1y_1
+		distance_change_1 = sqrt(delta_x_1 ** 2 + delta_y_1 ** 2)
 
-	for i in range(2,120,2):
-		p1x = far_field * math.sin(60)
-		p1y = far_field * math.cos(60)
-		p2x = far_field * math.sin(58)
-		p2y = far_field * math.cos(58)
-		delta_x = p2x - p1x
-		delta_y = p2y - p1y
-		distance_change = sqrt(delta_x ** 2 + delta_y ** 2)
-		point_list[z] = Next_Point()
+		point_list_arc1[z+1] = Next_Point(distance_change_1, i, point_list_arc1[z[0]],point_list_arc1[z[1]])
+		z = z + 1
+	z = 0								# variable for incrementing
+	point_list_arc2 = [0] * 60			# creates a list with 60 entries
+	point_list_arc2[0] = point_60
+	for i in range(90,152,2):
+		p1x_2 = (far_field + 10) * math.sin(60)				#lines 139-145 are used to find the necessary distance to travel to next point on the second arc
+		p1y_2 = (far_field + 10) * math.cos(60)
+		p2x_2 = (far_field + 10) * math.sin(58)
+		p2y_2 = (far_field + 10) * math.cos(58)
+		delta_x_2 = p2x_2 - p1x_2
+		delta_y_2 = p2y_2 - p1y_2
+		distance_change_2 = sqrt(delta_x_2 ** 2 + delta_y_2 ** 2)
+
+		point_list_arc2[z+1] = Next_Point(distance_change, i, point_list_arc2[z[0]],point_list_arc2[z[1]])
 		z = z + 1
 
+	z = 0								# variable for incrementing
+	point_list_arc3 = [0] * 60			# creates a list with 60 entries
+	point_list_arc3[0] = point_120
+	for i in range(148,88,-2):
+		p1x_3 = (far_field + 20) * math.sin(60)				#lines 139-145 are used to find the necessary distance to travel to next point on the third arc
+		p1y_3 = (far_field + 20) * math.cos(60)
+		p2x_3 = (far_field + 20) * math.sin(58)
+		p2y_3 = (far_field + 20) * math.cos(58)
+		delta_x_3 = p2x_3 - p1x_3
+		delta_y_3 = p2y_3 - p1y_3
+		distance_change_3 = sqrt(delta_x_3 ** 2 + delta_y_3 ** 2)
+
+		point_list_arc3[z+1] = Next_Point(distance_change_3, i, point_list_arc3[z[0]],point_list_arc3[z[1]])
+		z = z + 1
 
 
 ############################################################################################
