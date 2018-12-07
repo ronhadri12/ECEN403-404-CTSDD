78689976613b4189d2ad0c0789543738b262e29d
blakethompson82 <44252687+blakethompson82@users.noreply.github.com>
2018-12-06 18:20:43 -0600

    Add files via upload

--- Next_Point.py
+++ Next_Point.py
@@ -1,52 +1,46 @@
-# Script to determine next coordinate(lat, long), based on current
-# location, desired angle, desired distance
+distance = 10
+angle = 69
+lat_1 = 32.97979798
+long_1 = -97.5454545445
+altitude = 10
 
+from dronekit import VehicleMode, LocationGlobal
 import math
+#############################################################################################
+def Next_Point(distance, angle, lat1, long1, altitude):
+
+    lat2 = (lat1 * math.pi) / 180               #converts latitude angle to radians
+    long2 = (long1 * math.pi) / 180             #converts longitude angle to radians
 
 
-def Next_Point(distance, angle, lat1, long1):
-    #distance = float(input("Distance from current location (meters): "))
-    distance = 10
-    #angle = float(input("Angle from current location (degrees): "))
-    angle = 293
-
-    #lat1 = float(input("Current latitude: "))
-    #long1 = float(input("Current longitude: "))
-    lat1 = 32.776664
-    long1 = -96.7969879
-
-    ################################################################
-
-    #Calculates length between each degree of longitude, based on latitude coordinate
+    angle_rad = (angle * math.pi) / 180   #heading in radians
 
     arc_length_per_degree = 111317.4306 #arc length per degree of earth's circumference (meters)
 
-    long_difference = math.cos(lat1) * arc_length_per_degree
-
+    long_difference = math.cos(lat2) * arc_length_per_degree  #Calculates length between each degree of longitude, based on latitude coordinate
 
     ################################################################
 
-    dx = distance * math.sin(angle)     # Change in x direction
-    dy = distance * math.cos(angle)     # Change in y direction
+    dx = distance * math.cos(angle_rad)     # Change in x direction
+    dy = distance * math.sin(angle_rad)     # Change in y direction
 
-    delta_longitude = dx / (111131.5 * math.cos(lat1)) # Converts change in x to change in longitude [degrees]
+    delta_longitude = dx / long_difference      # Converts change in x to change in longitude
 
-        # Distance between each degree of latitude varies between 110949 [m] and 111314 [m].
-        # Average of the two used since distance differences of project is so small
+    delta_latitude = dy / 110863   # Converts change in y to change in latitude
+                                    # 110863 is the distance between latitude line at 30.594114.
+                                    # Constant because the variation in distance between latitude lines is so small
+                                    # it can be kept constant
 
-    delta_latitude = dy / long_difference   # Converts change in y to change in latitude [degrees]
+    long3 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
+    lat3 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
 
-    long2 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
-    lat2 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
 
-    list = [lat2,long2]
-
-    return list
-
+    return LocationGlobal(lat3,long3,altitude)
 
 
 
-point_1 = Next_Point(10,293,32.776664,-96.7969879)
 
-print(point_1[0])
-print(point_1[1])
+
+list_1 = Next_Point(336.61,33.2,30.594114,-96.3347661,10)
+
+print list_1
