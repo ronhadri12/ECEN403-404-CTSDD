5fe091f7efaf30bb5fd8748a8a81c3c0a27fea7c
blakethompson82 <44252687+blakethompson82@users.noreply.github.com>
2019-01-27 11:45:19 -0600

    Add files via upload

--- Flight_Control_1.py
+++ Flight_Control_1.py
@@ -5,39 +5,59 @@
 
 
 # Import Dronekit-Python
-from dronekit import connect, VehicleMode, time
+from dronekit import connect, VehicleMode, time, LocationGlobal
 import math
+import scipy.integrate as integrate
 
 
 ##############################################################################
 # Function that returns the next point to fly to
-def Next_Point(distance, angle, lat1, long1,altitude):
+def Next_Point(distance, angle, lat1, long1, altitude):
 
-    #Calculates length between each degree of longitude, based on latitude coordinate
-
-	arc_length_per_degree = 111317.4306 #arc length per degree of earth's circumference (meters)
-
-	long_difference = math.cos(lat1) * arc_length_per_degree	#dependent on latitude
+    lat2 = (lat1 * math.pi) / 180               #converts latitude angle to radians
+    long2 = (long1 * math.pi) / 180             #converts longitude angle to radians
 
 
+    lower_lat = math.floor(lat1)     #finds the latitude line above the current location
+    upper_lat = math.ceil(lat1)      #finds the latitude line below the current location
+
+    polar_lower_lat = (lower_lat * math.pi) / 180
+    polar_upper_lat = (upper_lat * math.pi) / 180
+
+    lower_angle = (math.atan((6378137.0/6356752.0) * math.tan(polar_lower_lat)))    #angle (polar) between equator and upper latitude line
+    upper_angle = (math.atan((6378137.0/6356752.0) * math.tan(polar_upper_lat)))    #angle (polar) between equator and upper latitude line
 
 
-	dx = distance * math.sin(angle)     # Change in x direction
-	dy = distance * math.cos(angle)     # Change in y direction
+    def integrand(x):
+        return math.sqrt(((6378137**2) * ((math.sin(x))**2)) + ((6356752**2) * ((math.cos(x))**2)))
 
-	delta_longitude = dx / (111131.5 * math.cos(lat1)) # Converts change in x to change in longitude [degrees]
+    lat_difference1 = integrate.quad(integrand, lower_angle, upper_angle)        # Integral to find the arc length between latitiude lines of the earth
 
-        # Distance between each degree of latitude varies between 110949 [m] and 111314 [m].
-        # Average of the two used since distance differences of project is so small
+    lat_difference2 = lat_difference1[0]       #integrate.quad outputs  a tuple with two items in the list, this pulls the first and leaves the error
 
-	delta_latitude = dy / long_difference   # Converts change in y to change in latitude [degrees]
+    angle_rad = (angle * math.pi) / 180   #heading in radians
 
-	long2 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
-	lat2 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
+    arc_length_per_degree = 111317.4306 #arc length per degree of longitude at Earth's equator (meters)
 
-	list = [lat2,long2,altitude]
+    long_difference = math.cos(lat2) * arc_length_per_degree  #Calculates length between each degree of longitude, based on latitude coordinate
 
-	return list
+
+    dx = distance * math.cos(angle_rad)     # Change in x direction
+    dy = distance * math.sin(angle_rad)     # Change in y direction
+
+    delta_longitude = dx / long_difference      # Converts change in x to change in longitude
+
+    delta_latitude = dy / lat_difference2   # Converts change in y to change in latitude
+                                    # 111220 is the distance between latitude lines 30 and 31.
+                                    # Constant because the variation in distance between latitude lines is so small
+                                    # it can be kept constant(110574 m - 111694 m)
+
+    long3 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
+    lat3 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
+
+
+    return LocationGlobal(lat3,long3,altitude)
+
 #####################################################################################################################
 # User input values
 antenna_frequency = float(input("Antenna Frequency: "))
@@ -75,12 +95,13 @@ print("Mode: %s") %vehicle.mode.name
 while not vehicle.armed:				#waits for vehicle to be armed
 	vehicle.armed = True
 	time.sleep(1)
-print("TEST")
+
 
 print("Armed: %s") %vehicle.armed
+time.sleep(2)
 
 print("Taking off")
-vehicle.simple_takeoff(5)				#Takes off and flies up five meters and holds
+vehicle.simple_takeoff(3)				#Takes off and flies up five meters and holds
 
 time.sleep(3)
 
@@ -114,7 +135,8 @@ elif heading > 240:
 	degree_left = heading - 120
 	degree_right = 0 + (360 - heading)
 
-point_1 = Next_Point(far_field, degree_left, lat_ant, long_ant,alt_ant) #calculates first point
+point_1 = Next_Point(far_field, degree_left, lat_ant, long_ant,alt_ant) #calculates first
+
 ################################################################################################
 # Autonomous Flight
 
@@ -132,7 +154,7 @@ while vehicle.channels['5'] < 1200:		#if Flight Mode = 2 on controller
 	z = 0								# variable for incrementing
 	point_list_arc1 = [0] * 60			# creates a list with 60 entries
 	point_list_arc1[0] = point_1
-	p1x_1 = far_field * math.sin(60)				#lines 128-136 are used to find the necessary distance to travel to next point on the first arc
+	p1x_1 = far_field * math.sin(60)	#lines 136-142 are used to find the necessary distance to travel to next point on the first arc
 	p1y_1 = far_field * math.cos(60)
 	p2x_1 = far_field * math.sin(58)
 	p2y_1 = far_field * math.cos(58)
@@ -144,7 +166,7 @@ while vehicle.channels['5'] < 1200:		#if Flight Mode = 2 on controller
 	if degree_left >= 90:
 		arc_degree_left = degree_left - 90
 	elif degree_left < 90:
-		arc_degree_left = 360 - (90 - degree_left)			#lines 138-145 are used to find the starting and ending angle (heading) that the drone will travel to
+		arc_degree_left = 360 - (90 - degree_left)			#lines 145-152 are used to find the starting and ending angle (heading) that the drone will travel to
 	if degree_right <= 270:
 		arc_degree_right = degree_right + 90
 	elif degree_right > 270:
--- Next_Point.py
+++ Next_Point.py
@@ -1,10 +1,6 @@
-distance = 10
-angle = 69
-lat_1 = 32.97979798
-long_1 = -97.5454545445
-altitude = 10
 
 from dronekit import VehicleMode, LocationGlobal
+import scipy.integrate as integrate
 import math
 #############################################################################################
 def Next_Point(distance, angle, lat1, long1, altitude):
@@ -13,23 +9,39 @@ def Next_Point(distance, angle, lat1, long1, altitude):
     long2 = (long1 * math.pi) / 180             #converts longitude angle to radians
 
 
+    lower_lat = math.floor(lat1)     #finds the latitude line above the current location
+    upper_lat = math.ceil(lat1)      #finds the latitude line below the current location
+
+    polar_lower_lat = (lower_lat * math.pi) / 180
+    polar_upper_lat = (upper_lat * math.pi) / 180
+
+    lower_angle = (math.atan((6378137.0/6356752.0) * math.tan(polar_lower_lat)))    #angle (polar) between equator and upper latitude line
+    upper_angle = (math.atan((6378137.0/6356752.0) * math.tan(polar_upper_lat)))    #angle (polar) between equator and upper latitude line
+
+
+    def integrand(x):
+        return math.sqrt(((6378137**2) * ((math.sin(x))**2)) + ((6356752**2) * ((math.cos(x))**2)))
+
+    lat_difference1 = integrate.quad(integrand, lower_angle, upper_angle)        # Integral to find the arc length between latitiude lines of the earth
+
+    lat_difference2 = lat_difference1[0]       #integrate.quad outputs  a tuple with two items in the list, this pulls the first and leaves the error
+
     angle_rad = (angle * math.pi) / 180   #heading in radians
 
-    arc_length_per_degree = 111317.4306 #arc length per degree of earth's circumference (meters)
+    arc_length_per_degree = 111317.4306 #arc length per degree of longitude at Earth's equator (meters)
 
     long_difference = math.cos(lat2) * arc_length_per_degree  #Calculates length between each degree of longitude, based on latitude coordinate
 
-    ################################################################
 
     dx = distance * math.cos(angle_rad)     # Change in x direction
     dy = distance * math.sin(angle_rad)     # Change in y direction
 
     delta_longitude = dx / long_difference      # Converts change in x to change in longitude
 
-    delta_latitude = dy / 110863   # Converts change in y to change in latitude
-                                    # 110863 is the distance between latitude line at 30.594114.
+    delta_latitude = dy / lat_difference2   # Converts change in y to change in latitude
+                                    # 111220 is the distance between latitude lines 30 and 31.
                                     # Constant because the variation in distance between latitude lines is so small
-                                    # it can be kept constant
+                                    # it can be kept constant(110574 m - 111694 m)
 
     long3 = long1 + delta_longitude         # Adds change in longitude to original longitude to give new longitude point
     lat3 = lat1 + delta_latitude            # Adds change in latitude to original latitude to give new latitude point
@@ -37,10 +49,11 @@ def Next_Point(distance, angle, lat1, long1, altitude):
 
     return LocationGlobal(lat3,long3,altitude)
 
+#############################################################################################################################
 
 
 
 
-list_1 = Next_Point(336.61,33.2,30.594114,-96.3347661,10)
+list_1 = Next_Point(337.98,32.5,30.594405,-96.334708,10)
 
 print list_1
